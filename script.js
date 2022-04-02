b4c125c1d5455b6a318515a2a75b9921fa4e0905
Júlio Anderson <81596847+kbaths@users.noreply.github.com>
2022-04-02 20:58:41 -0300

    mostrar aviso de palavra repetida

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ const technologies = [
 ];
 
 const secretWord = technologies[Math.floor(Math.random() * technologies.length)];
+const wrongLyrics = [];
+const correctLetters = [];
 
 // realiza a validação das teclas
 document.addEventListener("keydown", (event) => {
-    const code = event.keyCode; // 65 - 90 (intervalo entre A e Z) 
+    const code = event.keyCode; // 65 - 90 (intervalo entre A e Z)
+    // letra faz parte ou não da palavra
     if (isLyrics(code)) {
-
+        const lyrics = event.key;
+        if (wrongLyrics.includes(lyrics)) {
+            warningRepeatedLetter();
+        } else {
+            if (secretWord.includes(lyrics)) {
+                correctLetters.push(lyrics);
+            } else {
+                wrongLyrics.push(lyrics);
+            }
+        }
+        // atualizarJogo();
     }
-})
+});
+
+// mostrar aviso de letra repetida
+function warningRepeatedLetter() {
+    const warning = document.querySelector(".repeated-word warning");
+    warning.classList.add("show");
+    setTimeout(() => {
+        warning.classList.remove("show");
+    }, 1000);
+}
+
+function isLyrics(code) {
+    return code >= 65 && code <= 90;
+}
